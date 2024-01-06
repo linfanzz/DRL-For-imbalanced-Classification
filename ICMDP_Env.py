d1f2ebfc90bd30432bb5ded5d2394431e712ddac
linfanzz <zenglinfan@outlook.com>
2024-01-06 17:59:21 +0800

    modify env and model for pbcnn

--- ICMDP_Env.py
+++ ICMDP_Env.py
@@ -5,14 +5,13 @@ import gym
 from gym import error, spaces
 from gym import utils
 from gym.utils import seeding
-from sklearn.metrics import classification_report, confusion_matrix
-
+from sklearn.metrics import classification_report, confusion_matrix, precision_recall_fscore_support
+from imblearn.metrics import geometric_mean_score
 
 class ClassifyEnv(gym.Env):
 
-    def __init__(self, mode, imb_rate, trainx, trainy, ):  # mode means training or testing
+    def __init__(self, mode, trainx, trainy, ):  # mode means training or testing
         self.mode = mode
-        self.imb_rate = imb_rate
 
         self.Env_data = trainx
         self.Answer = trainy
@@ -22,6 +21,7 @@ class ClassifyEnv(gym.Env):
 
         self.num_classes = len(set(self.Answer))
         self.action_space = spaces.Discrete(self.num_classes)
+        self.observation_space = spaces.Box(low=0, high=1, shape=(20,256,1), dtype=np.float32)
         print(self.action_space)
         self.step_ind = 0
         self.y_pred = []
@@ -30,23 +30,23 @@ class ClassifyEnv(gym.Env):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
+    # return: (states, observations)
+    def reset(self):
+        if self.mode == 'train':
+            np.random.shuffle(self.id)
+        self.step_ind = 0
+        self.y_pred = []
+        return self.Env_data[self.id[self.step_ind]]
+
     def step(self, a):
         self.y_pred.append(a)
         y_true_cur = []
         info = {}
         terminal = False
         if a == self.Answer[self.id[self.step_ind]]:
-            if self.Answer[self.id[self.step_ind]] == 0:
-                reward = 1.
-            else:
-                reward = 1. * self.imb_rate
+            reward = 1
         else:
-            if self.Answer[self.id[self.step_ind]] == 0:
-                reward = -1.
-                if self.mode == 'train':
-                    terminal = True
-            else:
-                reward = -1. * self.imb_rate
+            reward = -1
         self.step_ind += 1
 
         if self.step_ind == self.game_len - 1:
@@ -56,38 +56,28 @@ class ClassifyEnv(gym.Env):
             terminal = True
 
         return self.Env_data[self.id[self.step_ind]], reward, terminal, info
-
-    def My_metrics(self, y_pre, y_true):
-        confusion_mat = confusion_matrix(y_true, y_pre)
+    @staticmethod
+    def My_metrics(y_pre, y_true):
+        # TODO(zenglf): 计算多分类问题的G_mean, F1, precision, recall, TP, TN, FP, FN
+        confusion_mtx = confusion_matrix(y_true, y_pre, labels=range(15))
         print('\n')
-        print(classification_report(y_true, y_pre))
-        conM = np.array(confusion_mat, dtype='float')
-        TP = conM[1][1]
-        TN = conM[0][0]
-        FN = conM[1][0]
-        FP = conM[0][1]
-        TPrate = TP / (TP + FN)  # 真阳性率
-        TNrate = TN / (TN + FP)  # 真阴性率
-        FPrate = FP / (TN + FP)  # 假阳性率
-        FNrate = FN / (TP + FN)  # 假阴性率
-        PPvalue = TP / (TP + FP)  # 阳性预测值
-        NPvalue = TN / (TN + FN)  # 假性预测值
+        print(classification_report(y_true=y_true, y_pred=y_pre, labels=range(15)))
 
-        G_mean = np.sqrt(TPrate * TNrate)
+        G_mean = geometric_mean_score(y_true, y_pre, average='macro')
 
-        Recall = TPrate = TP / (TP + FN)
-        Precision = PPvalue = TP / (TP + FP)
-        F_measure = 2 * Recall * Precision / (Recall + Precision)
-        print(confusion_mat)
+        Recall, Precision, F_measure, _ = precision_recall_fscore_support(y_true, y_pre, average='macro')
+
         res = 'G-mean:{}, F_measure:{}\n'.format(G_mean, F_measure)
         print(res)
         print()
         return G_mean, F_measure
 
-    # return: (states, observations)
-    def reset(self):
-        if self.mode == 'train':
-            np.random.shuffle(self.id)
-        self.step_ind = 0
-        self.y_pred = []
-        return self.Env_data[self.id[self.step_ind]]
+
+
+# test
+if __name__=='__main__':
+    y_true = [2, 0, 2, 2, 0, 1]
+    y_pre = [0, 0, 2, 2, 0, 2]
+    g_mean, f1 = ClassifyEnv.My_metrics(y_pre=y_pre, y_true=y_true)
+    print(f"g_mean: {g_mean}")
+    print(f"f1: {f1}")
--- get_model.py
+++ get_model.py
@@ -3,6 +3,7 @@ import keras
 import tensorflow as tf
 import numpy as np
 from keras.models import Model, Sequential
+from keras import layers
 from keras.layers import Input, Dense, Conv2D, MaxPooling2D, Flatten, Activation, Embedding
 from keras.optimizers import Adam, SGD
 from keras.layers import LSTM
@@ -32,4 +33,31 @@ def get_image_model(in_shape, output):
     model.add(Dense(output))
     return model
 
+# pbcnn param
+_pkt_num = 20
+_pkt_bytes = 256
+_num_class = 15
 
+def _text_cnn_block(x, filters, height, width, data_format='channels_last'):
+    x = layers.Conv2D(filters=filters, kernel_size=(height, width),
+                        strides=1, data_format=data_format)(x)
+    x = layers.BatchNormalization(axis=-1 if data_format == 'channels_last' else 1)(x)
+    x = layers.Activation(activation='relu')(x)
+    x = tf.reduce_max(x, axis=1, keepdims=False)
+    return x
+
+def get_pbcnn_model():
+    x = Input(shape=(_pkt_num, _pkt_bytes, 1))
+    # y = tf.reshape(x, shape=(-1, _pkt_num, _pkt_bytes, 1))
+    y = tf.reshape(x, shape=(-1, _pkt_num, _pkt_bytes, 1))
+    data_format = 'channels_last'
+    y1 = _text_cnn_block(y, filters=256, height=3, width=_pkt_bytes)
+    y2 = _text_cnn_block(y, filters=256, height=4, width=_pkt_bytes)
+    y3 = _text_cnn_block(y, filters=256, height=5, width=_pkt_bytes)
+    y = layers.concatenate(inputs=[y1, y2, y3], axis=-1)
+    y = layers.Flatten(data_format=data_format)(y)
+    y = layers.Dense(512, activation='relu')(y)
+    y = layers.Dense(256, activation='relu')(y)
+    # y = layers.Dense(128, activation='relu')(y)
+    y = layers.Dense(_num_class, activation='linear')(y)
+    return Model(inputs=x, outputs=y)
